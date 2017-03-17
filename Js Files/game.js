51ada267d5c9f5be2277c1e7d9168144559d920f
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2017-03-17 17:33:44 -0400

    COMMNETS ADDED

--- Js Files/game.js
+++ Js Files/game.js
@@ -20,49 +20,55 @@ var s=0;
 var m=0;
 var button;
 var Sound;
+// VAR DECLARE ENDS
 
 // STARTGAME FUNCTION(MAIN FUNCTION)
 function startGame() {
         bgmusic = new sound("Assets/Music/[EDGE OF LIFE]-Just Fly Away.wav");
         bgmusic.play();
         Sound = new sound("Assets/Music/Game Over.mp3");
-	obj2  = new component (200,2, "black", 0, 450);
-	obj3  = new component (200,1, "black", 0, 515);
-	bg    = new component (200, 720,"Assets/Pictures/ChartLayout.png ", 0, 0, "image");
-	bg2   = new component2(1020, 720,"Assets/Pictures/stage1_floor_only.png ", 0, 0, "image");
-	mob   = new component2(100,100, "blue", 85, 720);
-	mob2  = new component2(100,100, "blue", 400, 720);
-	mob3  = new component2(100,100, "blue", 700, 720);
-	stage = new component2(1020, 279,"Assets/Pictures/stage1stage_only.png ", 0, 0.5, "image");
-	obj   = new component3(1020,1, "black", 0, 280);
-        score = new component("30px", "Consolas", "white", 80, 595, "text");
-        button = new component4(20,20, "red", 970, 690);
-        myGameArea.start();
-	myGameArea2.start();
-}
-//LARGE GAME AREA
-var myGameArea = {
+		obj2  = new component (200,2, "black", 0, 450);
+		obj3  = new component (200,1, "black", 0, 515);
+		bg    = new component (200, 720,"Assets/Pictures/ChartLayout.png ", 0, 0, "image");
+		bg2   = new component2(1020, 720,"Assets/Pictures/stage1_floor_only.png ", 0, 0, "image");
+		mob   = new component2(100,100, "blue", 85, 720);
+		mob2  = new component2(100,100, "blue", 400, 720);
+		mob3  = new component2(100,100, "blue", 700, 720);
+		stage = new component2(1020, 279,"Assets/Pictures/stage1stage_only.png ", 0, 0.5, "image");
+		obj   = new component3(1020,1, "black", 0, 280);
+		score = new component("30px", "Consolas", "white", 80, 595, "text");
+		myGameArea.start();
+		myGameArea2.start();
+};
+//ENDS
+
+//LARGE GAME CANVAS AREA
+var myGameArea = 
+{
     canvas : document.createElement("canvas"),
-    start : function() {
+    start : function() 
+	{
         this.canvas.width = 1020;
         this.canvas.height = 720;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('mousedown', function () {
-            
-        });
 	},
-	clear : function() {
+	clear : function() 
+	{
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-	stop : function() {
+	stop : function() 
+	{
         clearInterval(this.interval);
     }
 	
 };
-//SMALL GAME AREA
-var myGameArea2 = {
+//ENDS
+
+//SMALL GAME CANVAS AREA
+var myGameArea2 = 
+{
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 200;
@@ -79,6 +85,8 @@ var myGameArea2 = {
         clearInterval(this.interval);
     }
 };
+//ENDS
+
 //  SMALL GAME AREA UNIVERSAL COMPONENT
 function component(width, height, color, x, y, type) {
     this.type = type;
@@ -89,11 +97,13 @@ function component(width, height, color, x, y, type) {
 	this.x = x;
 	this.y = y;
 	// IMAGE FUNCTION
-	if(type === "image"){
+	if(type === "image")
+	{
 		this.image = new Image();
 		this.image.src = color;
 	}
-	    this.update = function() {
+	    this.update = function() 
+		{
 			ctx = myGameArea2.context;
 			if (type === "image") 
 			{
@@ -102,18 +112,21 @@ function component(width, height, color, x, y, type) {
 				this.y, 
 				this.width, this.height);
 			}
-                        else if (type ==="text"){
-                            ctx.font = this.width + " " + this.height;
-                            ctx.fillStyle = color;
-                            ctx.fillText(this.text, this.x, this.y);
-                        }
+            else if (type ==="text")
+			{
+				ctx.font = this.width + " " + this.height;
+				ctx.fillStyle = color;
+				ctx.fillText(this.text, this.x, this.y);
+            }
 			else 
 			{
 				ctx.fillStyle = color;
 				ctx.fillRect(this.x, this.y, this.width, this.height);
 			}
 		};
-	this.crashWith = function(otherobj) {
+		//CRASH FUNCTION
+	this.crashWith = function(otherobj) 
+	{
         var myleft = this.x;
         var myright = this.x + (this.width);
         var mytop = this.y;
@@ -129,16 +142,19 @@ function component(width, height, color, x, y, type) {
         }
         return crash;
     };
-	this.newPos = function(){
+	this.newPos = function()
+	{
 		this.x +=this.speedX;
 		this.y +=this.speedY;
 	};
 	
-}
+};
+//ENDS
 
 // LARGE GAME AREA UNIVERSAL COMPONENT
-function component2(width, height, color, x, y, type) {
-   this.type = type;
+function component2(width, height, color, x, y, type) 
+{
+    this.type = type;
 	this.width = width;
 	this.height = height;
 	this.speedX =0;
@@ -146,11 +162,13 @@ function component2(width, height, color, x, y, type) {
 	this.x = x;
 	this.y = y;
 	// IMAGE FUNCTION
-	if(type === "image"){
+	if(type === "image")
+	{
 		this.image = new Image();
 		this.image.src = color;
 	}
-	    this.update = function() {
+	    this.update = function() 
+		{
 			ctx = myGameArea.context;
 			if (type === "image") 
 			{
@@ -159,14 +177,15 @@ function component2(width, height, color, x, y, type) {
 				this.y, 
 				this.width, this.height);
 			}
-                        
-			else 
+            else 
 			{
 				ctx.fillStyle = color;
 				ctx.fillRect(this.x, this.y, this.width, this.height);
 			}
 		};
-	this.crashWith = function(otherobj) {
+		//CREAH FUNCTION
+	this.crashWith = function(otherobj) 
+	{
         var myleft = this.x;
         var myright = this.x + (this.width);
         var mytop = this.y;
@@ -182,44 +201,54 @@ function component2(width, height, color, x, y, type) {
         }
         return crash;
     };
-	this.newPos = function(){
+	this.newPos = function()
+	{
 		this.x +=this.speedX;
 		this.y +=this.speedY;
 	};
 	
-}
-function component4(width, height, color, x, y) {
+};
+//ENDS
+
+/* button function 
+function component4(width, height, color, x, y) 
+{
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
     this.y = y;    
-    this.update = function() {
+    this.update = function() 
+	{
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
-    this.clicked = function() {
+    this.clicked = function() 
+	{
         pause();
     };
 }
-
+*/
 
 // SIMPLE UNIVERSAL COMPONENT
-function component3(width, height, color, x, y) {
+function component3(width, height, color, x, y) 
+{
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;    
-	this.update = function (){    
+	this.update = function ()
+	{    
 		ctx = myGameArea.context;
 		ctx.fillStyle = color;
 		ctx.fillRect(this.x, this.y, this.width, this.height);
-		
 	};
-}
+};
+//ENDS
 
+//KEY CODE FUNCTIONS
 window.addEventListener("keydown", onKeyDown);
 window.addEventListener("keyup", onKeyUp);
 
@@ -228,168 +257,206 @@ function onKeyDown(event)
     switch(event.keyCode)
 	{
             case 65: // a - lane one
-            {if (laneOnePressed === false) 
+            {
+				if (laneOnePressed === false)
+				{					
                     laneOnePressed = true;
                     beatPressed = false;
                     break;
-                };
+				}
+            };
             case 83: // s - lane two
-            {if (laneTwoPressed === false) 
-			laneTwoPressed = true;
-			beatPressed = false;					
-			break;
-                    };  
+            {
+				if (laneTwoPressed === false) 
+				{	
+					laneTwoPressed = true;
+					beatPressed = false;					
+					break;
+				}
+            };  
             case 68: // d - lane three
-            {if (laneThreePressed === false) 
-			laneThreePressed = true;
-			beatPressed = false;					
-			break;};
+            {
+				if (laneThreePressed === false) 
+				{
+					laneThreePressed = true;
+					beatPressed = false;					
+					break;
+				}
+			};
             case 32: // space bar - spawn mobs
-            {if (spawnMobs === false)
-			spawnMobs = true;
-			break;
-                        };
+            {
+				if (spawnMobs === false)
+				{
+					spawnMobs = true;
+					break;
+				}
+            };
 	}
     window.onkeydown = null;
-}
-
+};
 
 function onKeyUp(event)
 {
     switch(event.keyCode)
 	{
-            case 65:{ // a - lane one
+        case 65:
+			{ // a - lane one
                 laneOnePressed = false;
                 beatPressed = false;
                 break;
             }
-            case 83:{ // s - lane two
+        case 83:
+			{ // s - lane two
                 laneTwoPressed = false;
                 beatPressed = false;
                 break;
             }
-            case 68:{ // d - lane three
+        case 68:
+			{ // d - lane three
                 laneThreePressed = false;
                 beatPressed = false;
                 break;
             }
-            case 32:{ // space bar - spawn mobs
+        case 32:
+			{ // space bar - spawn mobs
                 spawnMobs = false;
                 break;
             }
 	}
-}
+};
+//KEY CODE FUNCTIONS ENDS
 
 // UPDATE GAMEAREA FUNCTION FOR LARGE CANVAS
-function updateGameArea() {
-	if (mob.crashWith(obj)|| (mob2.crashWith(obj))||(mob3.crashWith(obj))) {
+function updateGameArea() 
+{
+	if (mob.crashWith(obj)|| (mob2.crashWith(obj))||(mob3.crashWith(obj))) 
+	{
         myGameArea.stop();
     } 
-	else {
+	else 
+	{
 		myGameArea.clear();
 		bg2.newPos();
 		bg2.update();
-                button.update();
-		mob.y -= 1;
+		
+		mob.y -= 1;    //normal
 		mob.update();
 		mob.newPos();
-		obj.update();//normal
-                
-		mob2.y -= 1.5;
+		
+		obj.update();
+		
+        mob2.y -= 1.5; //fast
 		mob2.update();
 		mob2.newPos();
-		//fast
-		mob3.y -= 1;
+		
+		mob3.y -= 0.8;   //heavy
 		mob3.update();
 		mob3.newPos();
-		//heavy
+		
 		stage.update();
+		//LANE KEYPRESS IF STATEMENTS
 		if (laneOnePressed === true && beatPressed === true)
 			{
-                            mob.y = 0;
-                            myObstacles.splice(0,1);
-                            s++;
+                mob.y = 0;
+				myObstacles.splice(0,1);
+                s++;
 			}
 		else if (laneTwoPressed === true && beatPressed === true)
 			{
-                            mob2.y = 0;
-                            myObstacles.splice(0,1);
-                            s++;
+                mob2.y = 0;
+                myObstacles.splice(0,1);
+                s++;
 			}
 		else if (laneThreePressed === true && beatPressed === true)
 			{
-                            mob3.y = 0;
-                            myObstacles.splice(0,1);
-                            s++;
+                mob3.y = 0;
+                myObstacles.splice(0,1);
+                s++;
 			}
 		else if (spawnMobs === true)
 			{
-                            mob = new component2(100,100, "blue", 85, 720);
-                            mob2  = new component2(100,100, "blue", 400, 720);
-                            mob3  = new component2(100,100, "blue", 700, 720);
+                mob = new component2(100,100, "blue", 85, 720);
+                mob2  = new component2(100,100, "blue", 400, 720);
+                mob3  = new component2(100,100, "blue", 700, 720);
 			}
-            }
-}
-		
+    }
+};
+// UPDATE GAMEAREA FUNCTION FOR LARGE CANVAS ENDS 
 
 // UPDATE GAMEAREA FUNCTION FOR SMALL CANVAS
-function updateGameArea2() {
-	if (mob.crashWith(obj)|| (mob2.crashWith(obj))||(mob3.crashWith(obj))) {
+function updateGameArea2() 
+{
+	if (mob.crashWith(obj)|| (mob2.crashWith(obj))||(mob3.crashWith(obj))) 
+	{
         Sound.play();    
         bgmusic.stop();
         myGameArea.stop();
         return;
     } 
-	else {
+	else 
+	{
         myGameArea2.clear();
-	bg.newPos();
-	bg.update();
-	obj3.newPos();
-	obj3.update();
-	obj2.update();
+		bg.newPos();
+		bg.update();
+		obj3.newPos();
+		obj3.update();
+		obj2.update();
         myGameArea2.frameNo += 1;
-        if (myGameArea2.frameNo === 1 || everyinterval(40)) {
+        if (myGameArea2.frameNo === 1 || everyinterval(40)) 
+		{
             w = myGameArea2.canvas.width;
             h = myGameArea2.canvas.height - 200;
             myObstacles.push(new component(50, 15,"Assets/Pictures/red.png ",80,5, "image"));
         }
-        for (i = 0; i < myObstacles.length; i++) {
+        for (i = 0; i < myObstacles.length; i++) 
+		{
             myObstacles[i].y += 3;
             myObstacles[i].update();
             var beat = myObstacles[i];
             if (obj2.x + obj2.width >= beat.x && obj2.x <= beat.x + beat.y && obj2.y >= beat.y && obj2.y <= beat.y + beat.height)
-		{
-                    beatPressed = true;
+			{
+				beatPressed = true;
+			}
+			else if (obj3.x + obj3.width >= beat.x && obj3.x <= beat.x + beat.y && obj3.y >= beat.y && obj3.y <= beat.y + beat.height)
+			{
+				myObstacles.shift();
+			}
 		}
-		else if (obj3.x + obj3.width >= beat.x && obj3.x <= beat.x + beat.y && obj3.y >= beat.y && obj3.y <= beat.y + beat.height)
-		{
-                    myObstacles.shift();
-		}
+		score.update();    
+		score.text=" " + s;
 	}
-    score.update();    
-    score.text=" " + s;
-}
-}
-function sound(src){
+};
+// UPDATE GAMEAREA FUNCTION FOR SMALL CANVAS
+
+//FUNCTION FOR ALL SOUND COMPONENT
+function sound(src)
+{
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.setAttribute("perload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function()
+	{
         this.sound.play();
     };
-    this.stop = function (){
+    this.stop = function ()
+	{
         this.sound.pause();
     };
-}
-function everyinterval(n) {
+};
+//ENDS
+
+function everyinterval(n) 
+{
     if ((myGameArea2.frameNo / n) % 1 === 0) {return true;}
     return false;
 };
 
-function pause(){
+//GAME PAUSE FUNCTION
+function pause()
+{
     myGameArea.stop();
     myGameArea2.stop();
     bgmusic.stop();
