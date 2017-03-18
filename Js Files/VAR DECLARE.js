d9b4fa2ee5b4fe8f4e0e8d9a041ecedd3e0339f7
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2017-03-18 14:29:59 -0400

    Different Files
    Created Different files for Different functions.

---
+++ Js Files/VAR DECLARE.js
@@ -0,0 +1,22 @@
+/// VAR DECLARE
+var notes;
+var mob;
+var mob2;
+var mob3;
+var obj;
+var obj2;
+var stage;
+var bg;
+var bg2;
+var myObstacles = [];
+var laneOnePressed = false;
+var laneTwoPressed = false;
+var laneThreePressed = false;
+var spawnMobs = false;
+var beatPressed = false;
+var bgmusic;
+var score;
+var s=0;
+var m=0;
+var button;
+var Sound;
